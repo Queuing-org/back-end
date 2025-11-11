fc3f5fad3da0d3a98eae46f314c4e197d2508a10
PARK, Yeonjong <yeonjong.park@outlook.com>
2025-11-11 14:48:51 +0900

    feat: 공통 응답 추가

--- src/main/java/queuing/core/room/presentation/response/ListRoomResponse.java
+++ src/main/java/queuing/core/room/presentation/response/ListRoomResponse.java
@@ -2,12 +2,8 @@ package queuing.core.room.presentation.response;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-
 public record ListRoomResponse(
-    boolean hasNext,
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    Long nextId,
-    List<RoomSummaryResponse> rooms
+    List<RoomSummaryResponse> rooms,
+    boolean hasNext
 ) {
 }
