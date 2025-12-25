6e6fe4935d30b0dbd2155b8a7bc20b152087de3c
PARK, Yeonjong <yeonjong.park@outlook.com>
2025-12-25 23:42:25 +0900

    fix(room): 표현 계층 네이밍 명확화

--- src/main/java/queuing/core/room/presentation/response/MusicTagResponse.java
+++ src/main/java/queuing/core/room/presentation/response/MusicTagResponse.java
@@ -1,12 +1,12 @@
 package queuing.core.room.presentation.response;
 
-import queuing.core.room.application.model.MusicTagItem;
+import queuing.core.room.application.dto.MusicTagDto;
 
 public record MusicTagResponse(
     String slug,
     String name
 ) {
-    public static MusicTagResponse from(MusicTagItem item) {
-        return new MusicTagResponse(item.slug(), item.name());
+    public static MusicTagResponse from(MusicTagDto dto) {
+        return new MusicTagResponse(dto.slug(), dto.name());
     }
 }
