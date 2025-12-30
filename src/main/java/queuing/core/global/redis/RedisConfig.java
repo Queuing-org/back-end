99490a4b8047566618fdf0419f4c6deb4f479288
PARK, Yeonjong <yeonjong.park@outlook.com>
2025-12-30 12:44:59 +0900

    fix: 개발 환경 redirect_uri 및 쿠키 Domain 설정 수정 (#9)
    * fix: 개발 환경 redirect_uri 및 쿠키 Domain 설정 수정
    
    * cicd: 테스트 오류 수정
    
    * fix: 중복 설정 파일 삭제
    
    * style: 설정 파일 스타일 수정

--- src/main/java/queuing/core/global/redis/RedisConfig.java
+++ src/main/java/queuing/core/global/redis/RedisConfig.java
@@ -3,12 +3,14 @@ package queuing.core.global.redis;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @EnableRedisHttpSession
+@Profile("!test")
 public class RedisConfig {
     @Value("${spring.data.redis.host}")
     private String host;
--- src/main/java/queuing/core/global/security/RedisSessionConfig.java
+++
@@ -1,8 +0,0 @@
-package queuing.core.global.security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
-@Configuration
-@EnableRedisHttpSession
-public class RedisSessionConfig {}
